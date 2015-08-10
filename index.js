c3182060299b3dd2bc5f4e11458f162e12aaaa1e
Bogdan Chadkin <trysound@yandex.ru>
2015-08-10 08:42:12 +0300

    Added browser support

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-module.exports = assert;
+(function () {
+  if(typeof exports === 'object') {
+    module.exports = assert;
+  } else {
+    window.assert = assert;
+  }
 
-function assert(val, msg) {
-  if (!val)
-    throw new Error(msg || 'Assertion failed');
-}
+  function assert(val, msg) {
+    if (!val)
+      throw new Error(msg || 'Assertion failed');
+  }
 
-assert.equal = function assertEqual(l, r, msg) {
-  if (l != r)
-    throw new Error(msg || ('Assertion failed: ' + l + ' != ' + r));
-};
+  assert.equal = function assertEqual(l, r, msg) {
+    if (l != r)
+      throw new Error(msg || ('Assertion failed: ' + l + ' != ' + r));
+  };
+} ());
