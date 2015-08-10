645682e219bce96f3b75846e951466cd82d6a067
Bogdan Chadkin <trysound@yandex.ru>
2015-08-10 10:52:39 +0300

    Better output

--- index.js
+++ index.js
@@ -7,11 +7,24 @@
 
   function assert(val, msg) {
     if (!val)
-      throw new Error(msg || 'Assertion failed');
+      throwAssertion(msg || (String(val) + ' == ' + true), val, true, '==', assert);
   }
 
   assert.equal = function assertEqual(l, r, msg) {
     if (l != r)
-      throw new Error(msg || ('Assertion failed: ' + l + ' != ' + r));
+      throwAssertion(msg || (JSON.stringify(l) + ' == ' + JSON.stringify(r)), l, r, '==', assertEqual);
   };
+
+  function throwAssertion(message, actual, expected, operator, stackStartFunction) {
+    var err = new Error(message);
+    err.name = 'Assertion failed';
+    err.actual = actual;
+    err.expected = expected;
+    err.operator = operator;
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(err, stackStartFunction);
+    }
+
+    throw err;
+  }
 } ());
